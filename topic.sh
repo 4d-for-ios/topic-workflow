49f532adabfec4ff11511b997dd7c9586bc5010c
Eric Marchand <phimage@users.noreply.github.com>
2019-11-29 14:44:14 +0100

    🐛 Sort repositories in JSON by ID

--- topic.sh
+++ topic.sh
@@ -1,2 +1,2 @@
 topic=$1
-curl -H "Accept: application/vnd.github.mercy-preview+json" https://api.github.com/search/repositories?q=topic:$topic | jq -S 'del(.. |.score?)' > $topic.json
+curl -H "Accept: application/vnd.github.mercy-preview+json" https://api.github.com/search/repositories?q=topic:$topic | jq -S 'del(.. |.score?)' | jq '.items|=sort_by(.id)' > $topic.json
