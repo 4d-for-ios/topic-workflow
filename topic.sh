48d1150c41f2886fcbcf75a9afa68f64ae2a8bb4
Eric Marchand <phimage@users.noreply.github.com>
2019-12-10 10:42:50 +0100

    🔐 Add token when using API. Add also a retry

--- topic.sh
+++ topic.sh
@@ -1,2 +1,13 @@
-topic=$1
-curl -H "Accept: application/vnd.github.mercy-preview+json" https://api.github.com/search/repositories?q=topic:$topic | jq -S 'del(.. |.score?)' | jq '.items|=sort_by(.id)' > $topic.json
+function search_topic {
+  topic=$1
+  token=$2
+  curl -H "Accept: application/vnd.github.mercy-preview+json" -H "Authorization: token $token" https://api.github.com/search/repositories?q=topic:$topic | jq -S 'del(.. |.score?)' | jq '.items|=sort_by(.id)' > $topic.json
+}
+search_topic $1 $2
+
+# if incomplete, try again
+incomplete_results=$(jq -r '.incomplete_results' a.json | grep "true" | wc -l)
+if [ $incomplete_results -eq 1 ]; then
+   sleep 10 # after 10s
+   search_topic $1 $2
+fi
