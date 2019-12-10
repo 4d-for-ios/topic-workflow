087422ba59511b5ffacdb646bae6c1142be2e5fc
Eric Marchand <phimage@users.noreply.github.com>
2019-12-10 10:44:31 +0100

    🔐Provide token to topic.sh script

--- topics.sh
+++ topics.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 currentDir="."
+token=$1
 input="$currentDir/topics.txt"
 while IFS= read -r line
 do
   echo "$line"
-  $currentDir/topic.sh $line
+  $currentDir/topic.sh $line $token
 done < "$input"
