f7cd2de8466ce6167ecb57dc54cb502ca20b31eb
eric.marchand@4d.com <eric.marchand@4d.com>
2020-01-08 19:23:05 +0100

    🐛 Fix path of files. Allow to pass it as argument or use current directory. Then use exit code when failing

--- Sources/flatten/main.swift
+++ Sources/flatten/main.swift
@@ -1,11 +1,19 @@
 import topic_workflow
 import FileKit
+import Foundation
 
-let workingPath: Path = "/Users/phimage/topic-workflow/"
+let args = CommandLine.arguments
+
+var workingPath: Path = .current
+if args.count > 1 {
+    workingPath = Path(rawValue: CommandLine.arguments[1])
+}
 do {
     try Generate.run(workingPath)
 } catch let error as FileKitError {
     print("\(error) \(String(describing: error.error))")
+    exit(1)
 } catch {
     print("\(error)")
+    exit(2)
 }
