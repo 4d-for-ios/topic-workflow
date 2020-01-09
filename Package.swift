ef4663d88db60f68e06069742be12fc6b3c7e5a7
Eric Marchand <eric.marchand@4d.com>
2020-01-09 13:00:30 +0100

    Prepare code to send mail and install SSL

--- Package.swift
+++ Package.swift
@@ -7,20 +7,19 @@ let package = Package(
     name: "topic-workflow",
     products: [
         .executable(name: "topic-workflow", targets: ["topic-workflow"]),
-        .library(
-            name: "TopicWorkflowKit",
-            targets: ["TopicWorkflowKit"])
+        .library(name: "TopicWorkflowKit", targets: ["TopicWorkflowKit"])
     ],
     dependencies: [
         .package(url: "https://github.com/nvzqz/FileKit.git", from: "6.0.0"),
         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.0"),
         .package(url: "https://github.com/Carthage/Commandant.git", .upToNextMinor(from: "0.16.0")),
-        .package(url: "https://github.com/jpsim/Yams.git", .upToNextMinor(from: "2.0.0"))
+        .package(url: "https://github.com/jpsim/Yams.git", .upToNextMinor(from: "2.0.0")),
+        .package(url: "https://github.com/twof/VaporMailgunService.git",  .upToNextMinor(from: "1.8.1"))
     ],
     targets: [
         .target(
             name: "TopicWorkflowKit",
-            dependencies: ["FileKit", "SwiftyJSON", "Commandant", "Yams"]),
+            dependencies: ["FileKit", "SwiftyJSON", "Commandant", "Yams", "Mailgun"]),
         .target(
             name: "topic-workflow",
             dependencies: ["TopicWorkflowKit"])
--- Sources/TopicWorkflowKit/Commands/GenerateCommand.swift
+++ Sources/TopicWorkflowKit/Commands/GenerateCommand.swift
@@ -3,6 +3,9 @@ import SwiftyJSON
 import Result
 import Foundation
 import Commandant
+import Mailgun
+import Vapor
+import Service
 
 struct GenerateCommand: CommandProtocol {
     typealias Options = GenerateOptions
@@ -21,7 +24,7 @@ struct GenerateCommand: CommandProtocol {
         //let config = Config(options: options) ?? Config.default
         let builder = Generate()
         do {
-            try builder.run(Path(rawValue: workDirectoryString))
+            try builder.run(Path(rawValue: workDirectoryString), apiKey: options.apiKey, email: options.email)
         } catch let error as FileKitError {
             print("\(error) \(String(describing: error.error))")
             exit(1)
@@ -39,12 +42,15 @@ struct GenerateOptions: OptionsProtocol {
 
     let path: String?
     let email: String?
+    let apiKey: String?
     let configurationFile: String?
 
-    static func create(_ path: String?) -> (_ email: String?) -> (_ config: String?) -> GenerateOptions {
+    static func create(_ path: String?) -> (_ email: String?)  -> (_ apiKey: String?) -> (_ config: String?) -> GenerateOptions {
         return { email in
-            return { config in
-                self.init(path: path, email: email, configurationFile: config)
+            return { apiKey in
+                return { config in
+                    self.init(path: path, email: email, apiKey: apiKey, configurationFile: config)
+                }
             }
         }
     }
@@ -53,6 +59,7 @@ struct GenerateOptions: OptionsProtocol {
         return create
             <*> mode <| Option(key: "path", defaultValue: nil, usage: "project root directory")
             <*> mode <| Option(key: "email", defaultValue: nil, usage: "email to send new")
+            <*> mode <| Option(key: "apiKey", defaultValue: nil, usage: "apiKey to send mail using mailgun")
             <*> mode <| Option(key: "config", defaultValue: nil, usage: "the path to configuration file")
     }
 }
@@ -71,14 +78,14 @@ extension Config {
 }
 
 public class Generate {
-    public func run(_ workingPath: Path) throws {
+    public func run(_ workingPath: Path, apiKey: String?, email: String?) throws {
         let topics = try Topic.readTopics(workingPath)
 
+        var news: [[String: Any]] = []
         for topic in topics {
             print("🏷 \(topic.name)")
             let json = try topic.jsonItems(at: workingPath)
             print("⏲ \(json["total_count"])")
-            let news: [JSON] = []
             for itemJson in json["items"].arrayValue {
                 if let fullName = itemJson["full_name"].string {
                     print(" 📦 \(fullName)")
@@ -93,13 +100,41 @@ public class Generate {
                             try orgaPath.createDirectory()
                         }
                         print("  👶 NEW")
-                        news.append(itemJson)
+                        if let data = itemJson.dictionaryObject {
+                            news.append(data)
+                        }
                     }
                     try DataFile(path: topicPath).write(itemJson.rawData())
                 }
             }
-            if !news.isEmpty {
-                print("🎉 There is new packages")
+        }
+
+        if !news.isEmpty {
+             print("🎉 There is new packages")
+
+            if let apiKey = apiKey {
+                /*   let domain = "sandbox01fc72c22b9a484eb7ffebbb1d590b73.mailgun.org"
+                 let mailgun = Mailgun(apiKey: apiKey, domain: domain, region: .eu)
+                 let message = Mailgun.TemplateMessage(
+                 from: "eric.marchand@4d.com",
+                 to: email ?? "eric.marchand@4d.com",
+                 subject: "News 4d-for-ios repositories",
+                 template: "new-repository",
+                 templateData: ["repositories": ""]
+                 )
+                 let app = try Application()
+                 let future = try mailgun.send(message, on: app)
+                 let semaphore = DispatchSemaphore(value: 0)
+                 future.whenSuccess { value in
+                 print("send email success")
+                 }
+                 future.whenFailure { error in
+                 print("\(error)")
+                 }
+                 future.whenComplete {
+                 semaphore.signal()
+                 }
+                 semaphore.wait()*/
             }
         }
     }
