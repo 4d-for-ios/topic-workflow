790fd6b6dd6d14105345da89a9e4c62a20655af0
Eric Marchand <59135882+mesopelagique@users.noreply.github.com>
2020-07-23 16:05:43 +0200

    Update push.sh

--- push.sh
+++ push.sh
@@ -4,5 +4,6 @@ GITHUB_TOKEN=$2
 echo "Push to branch $BRANCH"
 
 remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-
+git config --global user.email "action@github.com"
+git config --global user.name "GitHub Action"
 git push "${remote_repo}" HEAD:${BRANCH}
